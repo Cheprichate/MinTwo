d28bbdc4f405e11ba26cbf80bc178d69e18c0efc
Cheprichate <petrovich15071998@mail.ru>
2023-09-28 20:58:57 +0400

    Добавьте файлы проекта.

---
+++ MinTwo.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+int a;
+int b;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    std::cout << "Введите первое число : ";
+    std::cin >> a ;
+    std::cout << "Введите второе число : ";
+    std::cin >> b ;
+    if (a < b) {
+        std::cout << " Минимальное число : " << a;
+} else if (a > b) {
+        std::cout << " Минимальное число : " << b;
+} else {
+        std::cout << " Числа равны ";
+    }
+}
